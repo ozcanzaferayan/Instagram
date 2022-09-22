0704be6f81c3fef6f11a7b0fc57e33014286db4d
Zafer AYAN <ozcanzaferayan@gmail.com>
2022-09-22 21:22:07 +0300

    Add fonts

--- src/screens/Post/PostActions.tsx
+++ src/screens/Post/PostActions.tsx
@@ -1,17 +1,15 @@
 import {StyleSheet} from 'react-native';
 import React from 'react';
-import Text from '../../components/Text';
-// import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const PostActions = () => {
   return (
-    <Text></Text>
-    // <Icon.Button
-    //   name="facebook"
-    //   backgroundColor="#3b5998"
-    //   onPress={console.log}>
-    //   Login with Facebook
-    // </Icon.Button>
+    <Icon.Button
+      name="facebook"
+      backgroundColor="#3b5998"
+      onPress={console.log}>
+      Login with Facebook
+    </Icon.Button>
   );
 };
 
